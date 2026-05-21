20ef861b8419072d5b3c3f79b06113363e8276bc
Joncy Mariam John <joncymariam@gmail.com>
2026-05-21 15:29:30 +0530

    Tasks on json, csv, xml

---
+++ 2026-05-21/user.py
@@ -0,0 +1,66 @@
+import json
+
+with open("users.json") as f:
+    data = json.load(f)
+
+print("Total Users:", len(data))
+
+# json to dataframe
+
+import pandas as pd
+
+df = pd.json_normalize(data)
+
+print(df.head())
+
+print(df.duplicated().sum())
+
+print(df.isnull().sum())
+
+#print(df["address.city"].value_counts())
+
+#print(df["company.name"].value_counts())
+
+# Email Validation
+
+import re
+
+for email in df["email"]:
+    if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+        print(email)
+
+# Save to CSV 
+df.to_csv("users.csv", index=False)
+
+#Save to xml
+import xml.etree.ElementTree as ET
+
+root = ET.Element("users")
+
+for user in data:
+    user_elem = ET.SubElement(root, "user")
+
+    for key, value in user.items():
+
+        if isinstance(value, dict):
+            nested = ET.SubElement(user_elem, key)
+
+            for k, v in value.items():
+
+                if isinstance(v, dict):
+                    subnested = ET.SubElement(nested, k)
+
+                    for sk, sv in v.items():
+                        child = ET.SubElement(subnested, sk)
+                        child.text = str(sv)
+                else:
+                    child = ET.SubElement(nested, k)
+                    child.text = str(v)
+
+        else:
+            child = ET.SubElement(user_elem, key)
+            child.text = str(value)
+
+tree = ET.ElementTree(root)
+tree.write("users.xml")
+
